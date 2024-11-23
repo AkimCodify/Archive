14c5ee93459b91f36f5a6afe74da469016484de1
AkimCodify <akimmurzagaziev@gmail.com>
2024-11-23 18:09:50 +0400

    my changes

--- src/components/QuestionItem.jsx
+++ src/components/QuestionItem.jsx
@@ -14,17 +14,27 @@ const QuestionItem = () => {
     const [clickedRadio, setClickedRadio] = useState(false);
     const [isNext, setIsNext] = useState(false)
     const [answered, setAnswered] = useState(false)
+    const [stopTimer, setStopTimer] = useState(false)
+    const [prevvTime, setPrevTime] = useState(null)
 
     useEffect(() => {
         getQuestion(id);
     }, [id]);
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            updateTimer(timer)
-        }, 1000);
-    }, []);
+        checkTimer()
+        updateTimer()
+    }, [stopTimer]);
     
+    const checkTimer = () => {
+      if (!stopTimer) {
+        return timer = setInterval(() => {
+          updateTimer(timer);
+        }, 1000);
+    } else {
+      return timer = 0
+    }
+    }
 
     const handleSubmit = () => {
         setAnswered(true)
@@ -34,70 +44,95 @@ const QuestionItem = () => {
         } else {
             setIsCorrect(false)
         }
+
+        if (error === 2) {
+          setStopTimer(true)
+        }
     };
 
     const updateTimer = (timer) => {
         setTime((prevTime) => {
             if (prevTime <= 0) {
                 clearInterval(timer);
+                setStopTimer(true)
                 return 0;
             }
-            return prevTime - 1;
+            if (stopTimer === false) {
+              return prevTime - 1;
+            } else if (stopTimer === true) {
+              if (prevTime !== NaN) {
+                setPrevTime(prevTime)
+              } else {
+                return prevvTime
+              }
+            }
         });
-        return () => clearInterval(timer);
     }
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
+    let timer;
 
     return (
-        <div className='question-wrap'>
-            <div className='container'>
-                <div className='q-upper_info'>
-                    <ul className='q-list'>
-                        <li className='q-child'>Билет 1</li>
-                        <div className='q-otherstats'>
-                            <li className='q-child1'>Ошибки {error}</li>
-                            <li className='q-child'>Вопрос {questionNumber}/20</li>
-                            <li className='q-child'>Время: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</li>
-                        </div>
-                    </ul>
+      <div className="question-wrap">
+        <div className="container">
+          <div className="q-upper_info">
+            <ul className="q-list">
+              <li className="q-child">Билет 1</li>
+              <div className="q-otherstats">
+                <li className="q-child1">Ошибки {error}/2</li>
+                <li className="q-child">Вопрос {questionNumber}/20</li>
+                <li className="q-child">
+                  Время: {stopTimer ? `${Math.floor(prevvTime / 60)}:${prevvTime % 60}` : `${minutes}:${seconds}`}
+                </li>
+              </div>
+            </ul>
+          </div>
+          {questionNumber !== 20 && error !== 2 && !stopTimer ? <div className="question-div">
+            <div className="q-div">
+              <p className="q-question">{question?.question}</p>
+              {question?.image && (
+                <img
+                  src={question?.image}
+                  alt="Question Image"
+                  className="q-img"
+                />
+              )}
+            </div>
+            <div className="q-radio_wrap">
+              {question?.answers?.map((el, idx) => (
+                <div key={idx} className="q-radio_div">
+                  <input
+                    type="radio"
+                    name={`input-${index}`}
+                    checked={selectedAnswer?.answer === el.answer}
+                    onChange={() => {
+                      setSelectedAnswer(el);
+                      setClickedRadio(true);
+                    }}
+                  />
+                  <label>{el.answer}</label>
                 </div>
-                <div className='question-div'>
-                    <div className='q-div'>
-                        <p className='q-question'>{question?.question}</p>
-                        {question?.image && <img src={question?.image} alt="Question Image" className='q-img' />}
-                    </div>
-                    <div className='q-radio_wrap'>
-                        {question?.answers?.map((el, idx) => (
-                            <div key={idx} className='q-radio_div'>
-                                <input
-                                    type="radio"
-                                    name={`input-${index}`}
-                                    checked={selectedAnswer?.answer === el.answer}
-                                    onChange={() => {
-                                        setSelectedAnswer(el)
-                                        setClickedRadio(true)
-                                        
-                                    }}
-                                />
-                                <label>{el.answer}</label>
-                            </div>
-                        ))}
-                    </div>
+              ))}
+            </div>
                         <div className="q-btn_wrap">
                             {
                                 answered && isNext === true && <div>
                                         <div className='q-feedback_wrap'>
-                                            <button className={selectedAnswer?.correctly ? "q-feedback_yes" : "q-feedback_no"}>{selectedAnswer?.correctly ? "Ответ Верный!" : "Не верный ответ!"}</button>
+                                            <button className={isCorrect ? "q-feedback_yes" : "q-feedback_no"}>{isCorrect ? "Ответ Верный!" : "Не верный ответ!"}</button>
                                             <button className='q-next' onClick={(e) => {
                                                 setQuestion(questionNumber + 1);
                                                 setIndex(index + 1);
                                                 setId(id + 1);
                                                 setIsNext(false);
-                                                !selectedAnswer?.correctly && setError(error + 1);
+                                                !isCorrect && setError(error + 1);
                                                 setSelectedAnswer(null);
                                                 setClickedRadio(false)
+                                                if (questionNumber === 19 || error === 1) {
+                                                  console.log('q #20');
+                                                  
+                                                    setStopTimer(true)
+                                                }
                                             }}>Следующий</button>
                                         </div>
                                     </div>
@@ -111,7 +146,9 @@ const QuestionItem = () => {
                             </button>
                         </div>
                 </div>
-            </div>
+            : <div className='res-box'>
+                
+              </div>} </div>
         </div>
     );
 };
